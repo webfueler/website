9e04e9db6f2c6dee101a41298dfcbe3580680ab4
webfueler <ramonallstar@gmail.com>
2022-02-12 11:18:45 +0000

    feat: build now is done by finding all markdown files in pages folder

--- build/index.ts
+++ build/index.ts
@@ -4,42 +4,29 @@ import { MarkdownFileService } from './services/MarkdownFileService';
 import { Converter } from 'showdown';
 import { ParametersFileService } from './services/ParametersFileService';
 import { Options } from 'html-webpack-plugin';
+import { FileSystemService } from './services/FileSystemService';
 
 const converter = new Converter();
 const PAGES_PATH = '../src/pages/';
 
-const indexMarkdownFile = new MarkdownFileService(PAGES_PATH + 'index.md').file;
-const indexParameters = new ParametersFileService(indexMarkdownFile);
-const indexFile = new HtmlFileService(
-	indexMarkdownFile,
-	converter,
-	PAGES_PATH
-);
+const files = FileSystemService.getFilesInFolder(PAGES_PATH, '**/*.md');
+console.log(files);
 
-const aboutMarkdownFile = new MarkdownFileService(PAGES_PATH + 'about.md').file;
-const aboutParameters = new ParametersFileService(aboutMarkdownFile);
-const aboutFile = new HtmlFileService(
-	aboutMarkdownFile,
-	converter,
-	PAGES_PATH
-);
+const pages: PageService[] = files.map((file) => {
+	const markdownFile = new MarkdownFileService(PAGES_PATH + file).file;
+	const parameters = new ParametersFileService(markdownFile);
+	const htmlService = new HtmlFileService(
+		markdownFile,
+		converter,
+		PAGES_PATH,
+	);
 
-const blogGettingStartedMarkdownFile = new MarkdownFileService(PAGES_PATH + 'blog/getting-started.md').file;
-const blogGettingStartedParameters = new ParametersFileService(blogGettingStartedMarkdownFile);
-const blogGettingStartedFile = new HtmlFileService(
-	blogGettingStartedMarkdownFile,
-	converter,
-	PAGES_PATH
-);
+	return new PageService('../src/template.ejs', htmlService, parameters)
+});
 
 const getPages = async (): Promise<Options[]> => {
-	const indexPageOptions = await new PageService('../src/template.ejs', indexFile, indexParameters).htmlPluginOptions();
-	const aboutPageOptions = await new PageService('../src/template.ejs', aboutFile, aboutParameters).htmlPluginOptions();
-	const blogGettingStartedOptions = await new PageService('../src/template.ejs', blogGettingStartedFile, blogGettingStartedParameters).htmlPluginOptions();
-
-	return [
-		indexPageOptions, aboutPageOptions, blogGettingStartedOptions
-	]
+	const promises = pages.map((page) => page.htmlPluginOptions());
+	return Promise.all(promises)
 }
 
 export default getPages
--- build/services/FileSystemService.ts
+++ build/services/FileSystemService.ts
@@ -1,4 +1,5 @@
 import * as path from 'path';
+import { GlobSync } from 'glob';
 
 class FileSystemService {
 	static absolutePath(_path: string): string {
@@ -9,6 +10,15 @@ class FileSystemService {
 		const absoluteBasePath = FileSystemService.absolutePath(basePath);
 		const absoluteFilenamePath = FileSystemService.absolutePath(_path);
 		return absoluteFilenamePath.replace(absoluteBasePath,'').replace('.md','').slice(1);
+	}
+
+	static getFilesInFolder(folder: string, pattern: string): string[] {
+		const absoluteBasePath = FileSystemService.absolutePath(folder);
+		const glob = new GlobSync(FileSystemService.absolutePath(folder + pattern));
+		return glob.found.map((file) => {
+			const relativeFilePage = file.replace(absoluteBasePath, '').slice(1);
+			return relativeFilePage;
+		})
 	}
 }
 
